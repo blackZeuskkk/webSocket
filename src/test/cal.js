6788d64f0808d4630ec082217c6572f5b282b01f
zhaokai.kk <zeuskkk@163.com>
2022-09-26 18:15:23 +0800

    feat: init remote

---
+++ src/test/cal.js
@@ -0,0 +1,9 @@
+const crypto = requrie('crypto');
+const magic = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+const secWebSocketKey = 'w4v7O6xFTi36lq3RNcgctw==';
+
+let secWebSOcketAccept = crypto.createHash('sha1')
+  .update(secWebSocketKey + magic)
+  .digest('base64');
+
+  console.log(secWebSOcketAccept);
