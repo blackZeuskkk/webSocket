216b77643a803d3ece7c2b1449e68abe02af37f9
zhaokai.kk <zeuskkk@163.com>
2022-09-26 18:37:44 +0800

    feat: add new file

--- src/test/index.js
+++ src/test/index.js
@@ -1,4 +1,5 @@
 console.log('rebase');
+<<<<<<< HEAD
 console.log('merge');
 
 // TODO: 添加代码
@@ -9,3 +10,6 @@ console.log('merge');
 // 添加功能
 // 添加功能
 // 添加功能
+=======
+console.log('merge');
+>>>>>>> a4fbb07 (feat: add new file)
