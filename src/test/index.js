a4a97913381ac6970066a2861c75a696540a6dc9
zhaokai.kk <zeuskkk@163.com>
2022-09-26 19:28:41 +0800

    feat: add function

--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,4 @@
 console.log('rebase');
-<<<<<<< HEAD
 console.log('merge');
 
 // TODO: 添加代码
@@ -10,6 +9,3 @@ console.log('merge');
 // 添加功能
 // 添加功能
 // 添加功能
-=======
-console.log('merge');
->>>>>>> a4fbb07 (feat: add new file)
